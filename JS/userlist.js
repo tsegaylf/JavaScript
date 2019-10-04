3cc449f424629111976836fd9cf9fee0a1646e12
Lidya Tsegay <lidyatsegay@outlook.com>
2019-10-04 15:42:04 -0400

    New functions -creating and inputtingtable

--- JS/userlist.js
+++ JS/userlist.js
@@ -1,19 +1,4 @@
-console.log("Connected Successfully!");
-
-function buttonClicked() {
-    var usernameCtrl = document.getElementById("username");
-    usernameCtrl.value = users[1].username;
-    var isadminCtrl = document.getElementById("isAdmin");
-    isadminCtrl.checked = users[1].isAdmin;
-}
-
-function button2Clicked(){
-    var usernameCtrl = document.getElementById("username");
-    var userInput = usernameCtrl.value;
-    console.log("Username is", userInput);
-}
-
-
+console.log("Connected!");
 
 var users = [
     {
@@ -50,3 +35,19 @@ var users = [
         "isAdmin": true
     }
 ]
+
+function refresh(){
+    var tbodyCtrl = document.getElementById("tbody");
+    tbodyCtrl.innerHTML ="";
+    for(var user of users){
+        var tr ="<tr>";
+        tr += `<td>${user.id}</td>`
+        tr += `<td>${user.username}</td>`
+        tr += `<td>${user.firstname}</td>`
+        tr += `<td>${user.lastname}</td>`
+        tr += `<td>${user.isAdmin}</td>`
+        tr += `<td>${user.isReviewer}</td>`
+        tr +="</tr>";
+        tbodyCtrl.innerHTML += tr;
+    }
+}
